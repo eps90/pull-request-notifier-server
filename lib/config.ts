a26d4b6d3099c23d50a48138537fc9dca5ce2d56
Jakub Turek <ja@kubaturek.pl>
2015-07-20 19:14:00 +0200

    Create simple config loader

---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+///<reference path="../typings/tsd.d.ts"/>
+
+import yaml = require('js-yaml');
+import fs = require('fs');
+
+export class Config {
+    config:any;
+
+    constructor() {
+        this.config = yaml.safeLoad(fs.readFileSync('config/config.yml', 'utf-8'));
+    }
+}
---
+++ test/config_test.ts
@@ -0,0 +1,32 @@
+///<reference path="../typings/tsd.d.ts"/>
+
+import config = require('./../lib/config');
+import mockFs = require('mock-fs');
+import chai = require('chai');
+var expect = chai.expect;
+
+describe('Config', () => {
+    afterEach(() => {
+        mockFs.restore();
+    });
+
+    it('should load config from file', () => {
+        var configContent = 'a: b\nc: d\n';
+        mockFs({
+            'config': {
+                'config.yml': mockFs.file({
+                    content: configContent
+                })
+            }
+        });
+
+        var configInstance = new config.Config();
+        var expectedConfig = {
+            a: 'b',
+            c: 'd'
+        };
+
+        expect(configInstance.config).to.have.property('a', 'b');
+        expect(configInstance.config).to.have.property('c', 'd');
+    });
+});
