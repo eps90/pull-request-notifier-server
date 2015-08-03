9e773a2321a4deb001b863b57467c5096cf2842e
Jakub Turek <ja@kubaturek.pl>
2015-08-03 21:46:46 +0200

    Handle pullrequest:updated payload

--- lib/server/webhook_handler.ts
+++ lib/server/webhook_handler.ts
@@ -5,6 +5,7 @@ import factories = require('./../factories');
 
 export class WebhookHandler {
     private static PULLREQUEST_CREATED = 'pullrequest:created';
+    private static PULLREQUEST_UPDATED = 'pullrequest:updated';
 
     static handlePayload(type: string, body: string) {
         var parsedBody = JSON.parse(body);
@@ -12,11 +13,19 @@ export class WebhookHandler {
             case this.PULLREQUEST_CREATED:
                 this.onPullRequestCreated(parsedBody);
                 break;
+            case this.PULLREQUEST_UPDATED:
+                this.onPullRequestUpdated(parsedBody);
+                break;
         }
     }
 
     private static onPullRequestCreated(body: any) {
         var newPullRequest = factories.PullRequestFactory.create(body.pullrequest);
         repositories.PullRequestRepository.add(newPullRequest);
+    }
+
+    private static onPullRequestUpdated(body: any) {
+        var pullRequest = factories.PullRequestFactory.create(body.pullrequest);
+        repositories.PullRequestRepository.update(pullRequest);
     }
 }
--- test/server/webhook_handler_test.ts
+++ test/server/webhook_handler_test.ts
@@ -52,4 +52,53 @@ describe('Webhook listener', () => {
         expect(pullRequests[0].title).to.eq('Title of pull request');
         expect(pullRequests[0].description).to.eq('Description of pull request');
     });
+
+    it('should update a pull request on pullrequest:updated', () => {
+        var eventType = 'pullrequest:updated';
+        var payload = {
+            pullrequest: {
+                "id" :  1 ,
+                "title" :  "Title of pull request" ,
+                "description" :  "Description of pull request" ,
+                "state" :  "OPEN" ,
+                "author" : {
+                    "username": "emmap1",
+                    "display_name": "Emma"
+                },
+                "destination" : {
+                    "branch" : {  "name" :  "master" },
+                    "repository" : {
+                        "full_name": "team_name/repo_name",
+                        "name": "repo_name"
+                    }
+                },
+                "participants" : [
+                    // @todo
+                ],
+                "links": {
+                    "self": {
+                        "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
+                    }
+                }
+            }
+        };
+        var payloadString = JSON.stringify(payload);
+
+        var sampleProject = new models.Project();
+        sampleProject.fullName = 'team_name/repo_name';
+
+        var samplePr = new models.PullRequest();
+        samplePr.id = 1;
+        samplePr.title = 'This is sample title';
+        samplePr.state = models.PullRequestState.Declined;
+        samplePr.targetRepository = sampleProject;
+        repositories.PullRequestRepository.pullRequests['team_name/repo_name'] = [samplePr];
+
+        webhookHandler.WebhookHandler.handlePayload(eventType, payloadString);
+
+        var pullRequests = repositories.PullRequestRepository.findAll();
+        expect(pullRequests.length).to.eq(1);
+        expect(pullRequests[0].title).to.eq('Title of pull request');
+        expect(pullRequests[0].state).to.eq(models.PullRequestState.Open);
+    });
 });
