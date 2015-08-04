691349bf0b690079ba1671b5b6fc2d87c16e1ca8
Jakub Turek <ja@kubaturek.pl>
2015-08-04 18:38:59 +0200

    Move PullRequest events handling into separate class

--- lib/server/event_payload_handler.ts
+++ lib/server/event_payload_handler.ts
@@ -4,30 +4,45 @@ import repositories = require('./../repositories');
 import factories = require('./../factories');
 import logger = require('./../logger');
 
-export class EventPayloadHandler {
-    private static PULLREQUEST_CREATED = 'pullrequest:created';
-    private static PULLREQUEST_UPDATED = 'pullrequest:updated';
+export interface HandlerInterface {
+    supportedEvents: Array<string>;
+    handlePayload(type: string, bodyDecoded: any): void;
+}
 
-    private static PULLREQUEST_FULFILLED = 'pullrequest:fulfilled';
-    private static PULLREQUEST_REJECTED = 'pullrequest:rejected';
+export class PullRequestHandler implements HandlerInterface {
+    supportedEvents:Array<string> = [
+        'pullrequest:created',
+        'pullrequest:updated',
+        'pullrequest:fulfilled',
+        'pullrequest:rejected',
+        'pullrequest:approved',
+        'pullrequest:unapproved',
+    ];
 
-    private static PULLREQUEST_APPROVED = 'pullrequest:approved';
-    private static PULLREQUEST_UNAPPROVED = 'pullrequest:unapproved';
+    private PULLREQUEST_CREATED = 'pullrequest:created';
+    private PULLREQUEST_UPDATED = 'pullrequest:updated';
 
-    static handlePayload(type: string, body: string) {
-        var parsedBody = JSON.parse(body);
+    private PULLREQUEST_FULFILLED = 'pullrequest:fulfilled';
+    private PULLREQUEST_REJECTED = 'pullrequest:rejected';
+
+    private PULLREQUEST_APPROVED = 'pullrequest:approved';
+    private PULLREQUEST_UNAPPROVED = 'pullrequest:unapproved';
+
+    constructor() {}
+
+    handlePayload(type: string, bodyDecoded: any): void {
         switch (type) {
             case this.PULLREQUEST_CREATED:
-                this.onPullRequestCreated(parsedBody);
+                this.onPullRequestCreated(bodyDecoded);
                 break;
             case this.PULLREQUEST_UPDATED:
             case this.PULLREQUEST_APPROVED:
             case this.PULLREQUEST_UNAPPROVED:
-                this.onPullRequestUpdated(parsedBody);
+                this.onPullRequestUpdated(bodyDecoded);
                 break;
             case this.PULLREQUEST_FULFILLED:
             case this.PULLREQUEST_REJECTED:
-                this.onPullRequestClosed(parsedBody);
+                this.onPullRequestClosed(bodyDecoded);
                 break;
             default:
                 logger.info('Unhandled event payload: ' + type);
@@ -35,21 +50,37 @@ export class EventPayloadHandler {
         }
     }
 
-    private static onPullRequestCreated(body: any) {
+    onPullRequestCreated(body: any) {
         logger.info('Adding a pull request to the repository');
         var newPullRequest = factories.PullRequestFactory.create(body.pullrequest);
         repositories.PullRequestRepository.add(newPullRequest);
     }
 
-    private static onPullRequestUpdated(body: any) {
+    onPullRequestUpdated(body: any) {
         logger.info('Updating a pull request');
         var pullRequest = factories.PullRequestFactory.create(body.pullrequest);
         repositories.PullRequestRepository.update(pullRequest);
     }
 
-    private static onPullRequestClosed(body: any) {
+    onPullRequestClosed(body: any) {
         logger.info('Closing a pull request');
         var pullRequest = factories.PullRequestFactory.create(body.pullrequest);
         repositories.PullRequestRepository.remove(pullRequest);
+    }
+}
+
+export class EventPayloadHandler {
+    private static handlers: Array<HandlerInterface> = [
+        new PullRequestHandler()
+    ];
+
+    static handlePayload(type: string, bodyEncoded: string) {
+        var bodyDecoded = JSON.parse(bodyEncoded);
+        for (var handlerIndex = 0; handlerIndex < this.handlers.length; handlerIndex++) {
+            var handler: HandlerInterface = this.handlers[handlerIndex];
+            if (handler.supportedEvents.indexOf(type) !== -1) {
+                handler.handlePayload(type, bodyDecoded);
+            }
+        }
     }
 }
--- test/server/event_payload_handler_test.ts
+++ test/server/event_payload_handler_test.ts
@@ -8,165 +8,167 @@ import repositories = require('./../../lib/repositories');
 import eventPayloadHandler = require('./../../lib/server/event_payload_handler');
 
 describe('EventPayloadHandler', () => {
-    beforeEach(() => {
-        repositories.PullRequestRepository.pullRequests = {};
-    });
+    describe('PullRequestHandler', () => {
+        beforeEach(() => {
+            repositories.PullRequestRepository.pullRequests = {};
+        });
 
-    it('should create add new pull request to repository on pullrequest:created', () => {
-        var eventType = 'pullrequest:created';
-        var payload = {
-            pullrequest: {
-                "id" :  1 ,
-                "title" :  "Title of pull request" ,
-                "description" :  "Description of pull request" ,
-                "state" :  "OPEN" ,
-                "author" : {
-                    "username": "emmap1",
-                    "display_name": "Emma"
-                },
-                "destination" : {
-                    "branch" : {  "name" :  "master" },
-                    "repository" : {
-                        "full_name": "team_name/repo_name",
-                        "name": "repo_name"
-                    }
-                },
-                "participants" : [
-                    // @todo
-                ],
-                "links": {
-                    "self": {
-                        "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
-                    }
-                }
-            }
-        };
-
-        var payloadString = JSON.stringify(payload);
-        eventPayloadHandler.EventPayloadHandler.handlePayload(eventType, payloadString);
-
-        var pullRequests: Array<models.PullRequest> = repositories.PullRequestRepository.findAll();
-        expect(pullRequests.length).to.eq(1);
-        expect(pullRequests[0].author.username).to.eq('emmap1');
-        expect(pullRequests[0].author.displayName).to.eq('Emma');
-        expect(pullRequests[0].title).to.eq('Title of pull request');
-        expect(pullRequests[0].description).to.eq('Description of pull request');
-    });
-
-    function createUpdateLikePayload(eventKey: string) {
-        var payload = {
-            pullrequest: {
-                "id" :  1 ,
-                "title" :  "Title of pull request" ,
-                "description" :  "Description of pull request" ,
-                "state" :  "OPEN" ,
-                "author" : {
-                    "username": "emmap1",
-                    "display_name": "Emma"
-                },
-                "destination" : {
-                    "branch" : {  "name" :  "master" },
-                    "repository" : {
-                        "full_name": "team_name/repo_name",
-                        "name": "repo_name"
-                    }
-                },
-                "participants" : [
-                    // @todo
-                ],
-                "links": {
-                    "self": {
-                        "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
+        it('should create add new pull request to repository on pullrequest:created', () => {
+            var eventType = 'pullrequest:created';
+            var payload = {
+                pullrequest: {
+                    "id" :  1 ,
+                    "title" :  "Title of pull request" ,
+                    "description" :  "Description of pull request" ,
+                    "state" :  "OPEN" ,
+                    "author" : {
+                        "username": "emmap1",
+                        "display_name": "Emma"
+                    },
+                    "destination" : {
+                        "branch" : {  "name" :  "master" },
+                        "repository" : {
+                            "full_name": "team_name/repo_name",
+                            "name": "repo_name"
+                        }
+                    },
+                    "participants" : [
+                        // @todo
+                    ],
+                    "links": {
+                        "self": {
+                            "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
+                        }
                     }
                 }
-            }
-        };
-        var payloadString = JSON.stringify(payload);
+            };
 
-        var sampleProject = new models.Project();
-        sampleProject.fullName = 'team_name/repo_name';
+            var payloadString = JSON.stringify(payload);
+            eventPayloadHandler.EventPayloadHandler.handlePayload(eventType, payloadString);
 
-        var samplePr = new models.PullRequest();
-        samplePr.id = 1;
-        samplePr.title = 'This is sample title';
-        samplePr.state = models.PullRequestState.Declined;
-        samplePr.targetRepository = sampleProject;
-        repositories.PullRequestRepository.pullRequests['team_name/repo_name'] = [samplePr];
+            var pullRequests: Array<models.PullRequest> = repositories.PullRequestRepository.findAll();
+            expect(pullRequests.length).to.eq(1);
+            expect(pullRequests[0].author.username).to.eq('emmap1');
+            expect(pullRequests[0].author.displayName).to.eq('Emma');
+            expect(pullRequests[0].title).to.eq('Title of pull request');
+            expect(pullRequests[0].description).to.eq('Description of pull request');
+        });
 
-        eventPayloadHandler.EventPayloadHandler.handlePayload(eventKey, payloadString);
-
-        var pullRequests = repositories.PullRequestRepository.findAll();
-        expect(pullRequests.length).to.eq(1);
-        expect(pullRequests[0].title).to.eq('Title of pull request');
-        expect(pullRequests[0].state).to.eq(models.PullRequestState.Open);
-    }
-
-    it('should update a pull request on pullrequest:updated', () => {
-        var eventType = 'pullrequest:updated';
-        createUpdateLikePayload(eventType);
-    });
-
-    it('should update a pull request on pullrequest:approved', () => {
-        var eventType = 'pullrequest:approved';
-        createUpdateLikePayload(eventType);
-    });
-
-    it('should update a pull request on pullrequest:unapproved', () => {
-        var eventType = 'pullrequest:unapproved';
-        createUpdateLikePayload(eventType);
-    });
-
-    function createCloseLikePayload(eventKey: string) {
-        var payload = {
-            pullrequest: {
-                "id" :  1 ,
-                "title" :  "Title of pull request" ,
-                "description" :  "Description of pull request" ,
-                "state" :  "MERGED" ,
-                "author" : {
-                    "username": "emmap1",
-                    "display_name": "Emma"
-                },
-                "destination" : {
-                    "branch" : {  "name" :  "master" },
-                    "repository" : {
-                        "full_name": "team_name/repo_name",
-                        "name": "repo_name"
-                    }
-                },
-                "participants" : [
-                    // @todo
-                ],
-                "links": {
-                    "self": {
-                        "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
+        function createUpdateLikePayload(eventKey: string) {
+            var payload = {
+                pullrequest: {
+                    "id" :  1 ,
+                    "title" :  "Title of pull request" ,
+                    "description" :  "Description of pull request" ,
+                    "state" :  "OPEN" ,
+                    "author" : {
+                        "username": "emmap1",
+                        "display_name": "Emma"
+                    },
+                    "destination" : {
+                        "branch" : {  "name" :  "master" },
+                        "repository" : {
+                            "full_name": "team_name/repo_name",
+                            "name": "repo_name"
+                        }
+                    },
+                    "participants" : [
+                        // @todo
+                    ],
+                    "links": {
+                        "self": {
+                            "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
+                        }
                     }
                 }
-            }
-        };
-        var payloadString = JSON.stringify(payload);
+            };
+            var payloadString = JSON.stringify(payload);
 
-        var sampleProject = new models.Project();
-        sampleProject.fullName = 'team_name/repo_name';
+            var sampleProject = new models.Project();
+            sampleProject.fullName = 'team_name/repo_name';
 
-        var samplePr = new models.PullRequest();
-        samplePr.id = 1;
-        samplePr.targetRepository = sampleProject;
-        repositories.PullRequestRepository.pullRequests['team_name/repo_name'] = [samplePr];
+            var samplePr = new models.PullRequest();
+            samplePr.id = 1;
+            samplePr.title = 'This is sample title';
+            samplePr.state = models.PullRequestState.Declined;
+            samplePr.targetRepository = sampleProject;
+            repositories.PullRequestRepository.pullRequests['team_name/repo_name'] = [samplePr];
 
-        eventPayloadHandler.EventPayloadHandler.handlePayload(eventKey, payloadString);
+            eventPayloadHandler.EventPayloadHandler.handlePayload(eventKey, payloadString);
 
-        var pullRequests = repositories.PullRequestRepository.findAll();
-        expect(pullRequests.length).to.eq(0);
-    }
+            var pullRequests = repositories.PullRequestRepository.findAll();
+            expect(pullRequests.length).to.eq(1);
+            expect(pullRequests[0].title).to.eq('Title of pull request');
+            expect(pullRequests[0].state).to.eq(models.PullRequestState.Open);
+        }
 
-    it('should remove a pull request on pullrequest:fulfilled', () => {
-        var eventType = 'pullrequest:fulfilled';
-        createCloseLikePayload(eventType);
-    });
+        it('should update a pull request on pullrequest:updated', () => {
+            var eventType = 'pullrequest:updated';
+            createUpdateLikePayload(eventType);
+        });
 
-    it('should remove a pull request on pullrequest:rejected', () => {
-        var eventType = 'pullrequest:rejected';
-        createCloseLikePayload(eventType);
+        it('should update a pull request on pullrequest:approved', () => {
+            var eventType = 'pullrequest:approved';
+            createUpdateLikePayload(eventType);
+        });
+
+        it('should update a pull request on pullrequest:unapproved', () => {
+            var eventType = 'pullrequest:unapproved';
+            createUpdateLikePayload(eventType);
+        });
+
+        function createCloseLikePayload(eventKey: string) {
+            var payload = {
+                pullrequest: {
+                    "id" :  1 ,
+                    "title" :  "Title of pull request" ,
+                    "description" :  "Description of pull request" ,
+                    "state" :  "MERGED" ,
+                    "author" : {
+                        "username": "emmap1",
+                        "display_name": "Emma"
+                    },
+                    "destination" : {
+                        "branch" : {  "name" :  "master" },
+                        "repository" : {
+                            "full_name": "team_name/repo_name",
+                            "name": "repo_name"
+                        }
+                    },
+                    "participants" : [
+                        // @todo
+                    ],
+                    "links": {
+                        "self": {
+                            "href": "https://api.bitbucket.org/api/2.0/pullrequests/1"
+                        }
+                    }
+                }
+            };
+            var payloadString = JSON.stringify(payload);
+
+            var sampleProject = new models.Project();
+            sampleProject.fullName = 'team_name/repo_name';
+
+            var samplePr = new models.PullRequest();
+            samplePr.id = 1;
+            samplePr.targetRepository = sampleProject;
+            repositories.PullRequestRepository.pullRequests['team_name/repo_name'] = [samplePr];
+
+            eventPayloadHandler.EventPayloadHandler.handlePayload(eventKey, payloadString);
+
+            var pullRequests = repositories.PullRequestRepository.findAll();
+            expect(pullRequests.length).to.eq(0);
+        }
+
+        it('should remove a pull request on pullrequest:fulfilled', () => {
+            var eventType = 'pullrequest:fulfilled';
+            createCloseLikePayload(eventType);
+        });
+
+        it('should remove a pull request on pullrequest:rejected', () => {
+            var eventType = 'pullrequest:rejected';
+            createCloseLikePayload(eventType);
+        });
     });
 });
