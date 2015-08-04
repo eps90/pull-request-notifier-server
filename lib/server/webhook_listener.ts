38c836b97cbce79ef2821e600bd8422f30bbc99e
Jakub Turek <ja@kubaturek.pl>
2015-08-04 12:33:17 +0200

    Fix bug with parsing POST messages with event payload

--- lib/server/webhook_listener.ts
+++ lib/server/webhook_listener.ts
@@ -16,21 +16,26 @@ export class WebhookListener {
                     reqBody += chunk;
                 });
 
-                logger.info('Request decoded.');
+                req.on('end', () => {
+                    logger.info('Request decoded.');
 
-                if (req.headers.hasOwnProperty('x-event-key')) {
-                    var eventType = req.headers['x-event-key'];
-                    logger.info("Request with event payload '%s'", eventType);
-                    eventPayloadHandler.EventPayloadHandler.handlePayload(eventType, reqBody);
-                } else {
-                    logger.warn("Request does not contain 'x-event-key' header");
-                }
+                    if (req.headers.hasOwnProperty('x-event-key')) {
+                        var eventType = req.headers['x-event-key'];
+                        logger.info("Request with event payload '%s'", eventType);
+                        eventPayloadHandler.EventPayloadHandler.handlePayload(eventType, reqBody);
+                    } else {
+                        logger.warn("Request does not contain 'x-event-key' header");
+                    }
+
+                    res.writeHead(200, 'OK');
+                    res.end();
+                });
+            } else {
+                req.on('end', () => {
+                    res.writeHead(200, 'OK');
+                    res.end();
+                });
             }
-
-            req.on('end', () => {
-                res.writeHead(200, 'OK');
-                res.end();
-            });
         });
 
         server.listen(8888);
