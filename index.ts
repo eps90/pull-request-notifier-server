afe62cde2b9e98505d26b859fc1829d1d0ff5e77
Jakub Turek <ja@kubaturek.pl>
2015-08-06 19:57:24 +0200

    Start socket server on app connection

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@
 import repositories = require('./lib/repositories');
 import fetcher = require('./lib/fetcher');
 import webhook = require('./lib/server/webhook_listener');
+import socketServer = require('./lib/server/socket_server');
 
 import q = require('q');
 
 fetcher.Fetcher.initPullRequestCollection().then(() => {
     console.log(repositories.PullRequestRepository.findAll());
     webhook.WebhookListener.createServer();
+    socketServer.SocketServer.startSocketServer();
 }).catch((error) => {
    console.error(error);
 });
