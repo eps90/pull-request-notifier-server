7605a523cd7ee6bde61ffd5a08bd6786b750c29a
Jakub Turek <ja@kubaturek.pl>
2015-08-06 21:46:22 +0200

    Return Error instead of custom errors to provide better messages

--- lib/errors.ts
+++ lib/errors.ts
@@ -15,7 +15,7 @@ export class HttpRequestError extends BaseError {
         super();
     }
 
-    static throwError(url: string, response?: {statusCode?: number}, responseBody?: string): HttpRequestError {
+    static throwError(url: string, response?: {statusCode?: number}, responseBody?: string): Error {
         var message = 'Http request to ' + url + ' failed';
         var messageParts = [];
         if (response !== undefined && response.hasOwnProperty('statusCode')) {
@@ -30,7 +30,7 @@ export class HttpRequestError extends BaseError {
             message += ' ' + messageParts.join(' and ');
         }
 
-        return new HttpRequestError(message);
+        return new Error(message);
     }
 }
 
@@ -41,24 +41,24 @@ export class ConfigError extends BaseError {
         super();
     }
 
-    static throwFileNotFound(fileName: string): ConfigError {
+    static throwFileNotFound(fileName: string): Error {
         var message = "Config file at '" + fileName + "' not found";
-        return new ConfigError(message);
+        return new Error(message);
     }
 
-    static throwConfigPropertyRequired(propertyName: string): ConfigError {
+    static throwConfigPropertyRequired(propertyName: string): Error {
         var message = "Config property '" + propertyName + "' is required";
-        return new ConfigError(message);
+        return new Error(message);
     }
 
-    static throwConfigPropertyValueRequired(propertyName: string): ConfigError {
+    static throwConfigPropertyValueRequired(propertyName: string): Error {
         var message = "Config property '" + propertyName + "' cannot be empty";
-        return new ConfigError(message);
+        return new Error(message);
     }
 
-    static throwConfigPropertyHasWrongType(propertyName: string, expectedType: string, actualType: string): ConfigError {
+    static throwConfigPropertyHasWrongType(propertyName: string, expectedType: string, actualType: string): Error {
         var message = "Expected property '" + propertyName + "' to be type of '" + expectedType + "'" +
             ". Got '" + actualType + "' instead";
-        return new ConfigError(message);
+        return new Error(message);
     }
 }
