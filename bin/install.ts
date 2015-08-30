fd68bdc1eea0f89d3af82f10c7995eff25e15d8e
Jakub Turek <ja@kubaturek.pl>
2015-08-30 22:18:09 +0200

    Create installation script

---
+++ bin/install.ts
@@ -0,0 +1,16 @@
+///<reference path="../typings/tsd.d.ts"/>
+
+import installer = require('./../lib/installer/installer');
+var colors = require('colors');
+
+var installScript = new installer.Installer();
+try {
+    installScript.install();
+} catch (e) {
+    console.error(e.message);
+    console.error('Aborting'.red.bold);
+    process.exit(1);
+}
+
+console.log('Installation completed'.green.bold);
+
