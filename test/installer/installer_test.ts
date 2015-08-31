e5ff20a080eb3a16a77b9e4061aa598e2789bb57
Jakub Turek <ja@kubaturek.pl>
2015-08-31 08:17:25 +0200

    Write more meaningful tests

--- test/installer/installer_test.ts
+++ test/installer/installer_test.ts
@@ -10,6 +10,8 @@ import installerModule = require('./../../lib/installer/installer');
 describe.only('Installer', () => {
     afterEach(() => {
         mockFs.restore();
+        delete process.env['PARAM_AAA'];
+        delete process.env['PARAM_BBB'];
     });
 
     it('should process a config file when it doesn\'t exist', () => {
@@ -110,5 +112,53 @@ describe.only('Installer', () => {
         var actualConfig = jsYaml.safeLoad(fs.readFileSync(configPath, {encoding: 'utf8'}));
         expect(actualConfig['aaaa']).to.eq('predefined value');
         expect(actualConfig['bbbb']).to.eq(bParamValue);
+    });
+
+    it('should throw when mapped env var is does not exist', () => {
+        var templatePath = 'config/config.yml.dist';
+        var configPath = 'config/config.yml';
+        var aParam = 'PARAM_AAA';
+        var aParamValue = 'SET PARAM_AAA';
+        var bParam = 'PARAM_BBB';
+        var bParamValue = 'SET PARAM_BBB';
+
+        var paramFile = {
+            template: templatePath,
+            destination: configPath,
+            env_map: {
+                'aaaa': aParam,
+                'bbbb': bParam
+            }
+        };
+
+        var template = {
+            'aaaa': null,
+            'bbbb': null
+        };
+
+        process.env[aParam] = aParamValue;
+
+        var templateContent = jsYaml.dump(template);
+
+        mockFs({
+            "paramfile.json": mockFs.file({
+                content: JSON.stringify(paramFile)
+            }),
+            config: {
+                "config.yml.dist": mockFs.file({
+                    content: templateContent
+                })
+            }
+        });
+
+        var installer = new installerModule.Installer();
+        expect(() => { installer.install() }).to.throw();
+    });
+
+    it('should throw when paramfile.json does not exist', () => {
+        mockFs({});
+
+        var installer = new installerModule.Installer();
+        expect(() => { installer.install() }).to.throw();
     });
 });
