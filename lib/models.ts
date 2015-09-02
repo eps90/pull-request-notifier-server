16f2c16b679518ac961e9ed173ee544eb60a818f
Jakub Turek <ja@kubaturek.pl>
2015-09-02 19:32:10 +0200

    Remove todo from models

--- lib/models.ts
+++ lib/models.ts
@@ -25,7 +25,6 @@ export class PullRequestLinks {
     html: string;
 }
 
-// @todo Add link to Pull Request in browser
 export class PullRequest implements ModelInterface {
     id: number;
     title: string;
