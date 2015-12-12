d35110ddfee5f4c7f17726257a2f09f83157a279
Jakub Turek <ja@kubaturek.pl>
2015-12-12 12:01:55 +0100

    Log things with Loggly

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,10 +1,21 @@
 ///<reference path="../typings/tsd.d.ts"/>
 
 import winston = require('winston');
+require('winston-loggly');
+
+// Unfortunately, these things cannot be in config because it will cause circular reference errors
+var logglyToken = process.env['BBNOTIFIER_LOGGLY_TOKEN'];
+var logglySubdomain = process.env['BBNOTIFIER_LOGGLY_SUBDOMAIN'];
 
 var logger = new (winston.Logger)({
     transports: [
-        new (winston.transports.Console)()
+        new (winston.transports.Console)(),
+        new (winston.transports.Loggly)({
+            token: logglyToken,
+            subdomain: logglySubdomain,
+            tags: ["nodejs", "Bitbucket-Notifier"],
+            json: true
+        })
     ]
 });
 
