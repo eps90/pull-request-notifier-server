73bef5113850edc56173098cd64b2024731c7af4
Jakub Turek <ja@kubaturek.pl>
2016-12-11 13:23:33 +0100

    Clean up AbstractRepository

--- lib/repository/abstract_repository.ts
+++ lib/repository/abstract_repository.ts
@@ -5,37 +5,35 @@ import logger from './../logger';
 import {Config} from '../config';
 import {HttpRequestError} from "../errors";
 
+interface CollectionResponse {
+    next?: string;
+    values: any[];
+}
+
 export abstract class AbstractRepository {
     protected static requestForAll(requestUri: string, data: any[] = []) {
         const requestUrl = this.buildFullUrl(requestUri);
-        const requestConfig = this.buildRequestOptions();
 
         return q.Promise((resolve, reject) => {
-            logger.logHttpRequestAttempt(requestUrl);
-            request(requestUrl, requestConfig, (error, response, body)  => {
-                if (error || response.statusCode !== 200) {
-                    logger.logHttpRequestFailed(requestUrl);
-                    return reject(HttpRequestError.throwError(requestUrl, response, body));
-                }
-
-                logger.logHttpRequestSucceed(requestUrl);
-                const responseDecoded = JSON.parse(body);
-                data = data.concat(responseDecoded.values);
-
-                if (responseDecoded.hasOwnProperty('next')) {
-                    resolve(responseDecoded.next);
-                } else {
-                    resolve(null);
-                }
-            });
+            this.makeRequest(requestUrl)
+                .then((responseDecoded: CollectionResponse) => {
+                    data = data.concat(responseDecoded.values);
+                    const nextValue = responseDecoded.hasOwnProperty('next') ? responseDecoded.next : null;
+                    resolve(nextValue);
+                })
+                .catch((reason) => {
+                    reject(reason);
+                })
         }).then((responseUrl: string) => {
-            return responseUrl === null
-                ? data
-                : this.requestForAll(responseUrl, data);
+            return responseUrl === null ? data : this.requestForAll(responseUrl, data);
         });
     }
 
     protected static requestForOne(requestUrl: string) {
+        return this.makeRequest(requestUrl);
+    }
+
+    private static makeRequest(requestUrl) {
         const requestConfig = this.buildRequestOptions();
         return q.Promise((resolve, reject) => {
             logger.logHttpRequestAttempt(requestUrl);
