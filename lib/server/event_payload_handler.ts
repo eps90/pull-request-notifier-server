675d938db256c2713368b4022b72a9a8d7d6108d
Jakub Turek <ja@kubaturek.pl>
2017-02-20 20:47:26 +0100

    Add new handlers to EventPayloadHandler

--- lib/server/event_payload_handler.ts
+++ lib/server/event_payload_handler.ts
@@ -1,11 +1,16 @@
 import {EventDispatcher} from '../events/event_dispatcher';
 import * as q from 'q';
 import {HandlerInterface} from "./handler/handler";
-import {PullRequestHandler} from "./handler/pull_request_handler";
+import {PullRequestCreationHandler} from "./handler/pull_request_creation_handler";
+import {PullRequestUpdateHandler} from "./handler/pull_request_update_handler";
+import {PullRequestCloseHandler} from "./handler/pull_request_close_handler";
 
 export class EventPayloadHandler {
+    // @todo Allow to, somehow, inject handlers
     private static handlers: HandlerInterface[] = [
-        new PullRequestHandler()
+        new PullRequestCreationHandler(),
+        new PullRequestUpdateHandler(),
+        new PullRequestCloseHandler()
     ];
 
     static handlePayload(type: string, bodyEncoded: string): q.Promise<any> {
