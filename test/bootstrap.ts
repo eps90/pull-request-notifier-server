3eda2d1d35c71169672ebef773ae1894d28ed4fa
Jakub Turek <ja@kubaturek.pl>
2017-02-26 19:06:37 +0100

    Install sourcemaps on tests bootstrap

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -3,3 +3,5 @@ import winston = require('winston');
 
 logger.getLogger().remove(winston.transports.Console);
 logger.getLogger().remove(winston.transports.Loggly);
+
+require('source-map-support').install();
